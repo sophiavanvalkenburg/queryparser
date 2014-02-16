9ab9512ff995c15eca401f7d79f2138be62b8a95
sophiavv <meowypurr@yahoo.com>
2014-02-16 01:53:11 -0500

    put query text in POST request; remove user arg

--- NLQueryParser/QueryParser.php
+++ NLQueryParser/QueryParser.php
@@ -9,13 +9,10 @@ class QueryParser
      * make a call to python code that will parse the query
      * @return JSON encoded string response from python
      */
-    public static function parse($query_text, $user, $auth, $base_url=""){
+    public static function parse($query_text, $auth, $base_url=""){
         // test if args are not complete
         if ( empty($query_text) ){
             trigger_error("query is empty");
-        }
-        if ( empty($user) ){
-            trigger_error("user is empty");
         }
         if ( empty($auth) ){
             trigger_error("auth string is empty");
@@ -25,12 +22,15 @@ class QueryParser
         }
 
         // build url for API call
-        $data = array("text"=>$query_text, "user"=>$user, "auth"=>$auth);
-        $url = sprintf("%s?%s", $base_url, http_build_query($data) );
+        $get_data = array("auth"=>$auth);
+        $post_data = array("text"=>$query_text);
+        $url = sprintf("%s?%s", $base_url, http_build_query($get_data) );
 
         // build + send curl request
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_POST, count($post_data));
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
         $response = curl_exec($curl);
