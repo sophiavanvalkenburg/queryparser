9ccf75726cb2d08187b31353c025ff18533a7b3a
sophiavv <meowypurr@yahoo.com>
2014-02-23 19:05:13 -0500

    added logic to assign slot values

--- queryparser.py
+++ queryparser.py
@@ -10,7 +10,12 @@ return a JSON string of the parsed values.
 import json
 from flask import Flask, request
 from nltk import RegexpParser, word_tokenize, pos_tag
+from nltk.tree import Tree,ParentedTree
 from nltk.corpus import wordnet as wn
+
+
+UNK = "UNK"
+SKIP = "SKIP"
 
 app = Flask(__name__)
 
@@ -46,11 +51,9 @@ def parse(query_text, networks_json):
     tagged_result = t_cp.parse(domain_tagged)
     word_result = w_cp.parse(double_tokens)
     slots = assign_slots(tokens, tagged_result, word_result)
-    print 'tagged =',tagged
-    print 'tagged result = ',tagged_result
-    print 'word result =',word_result
-    print 'domain tagged =',domain_tagged
-    return { 'tagged':domain_tagged }
+    print 'tagged-result = ',tagged_result
+    print 'word-result = ',word_result
+    return slots
 
 def preprocess(string):
     """ 
@@ -72,28 +75,134 @@ def preprocess(string):
         new_string += ch
     return new_string
 
+def assign_slots(tokens, tag_tree, word_tree):
+    tokens_with_slot_tags = []
+    word_tree = ParentedTree.convert(word_tree)
+    tag_tree = ParentedTree.convert(tag_tree)
+    word_tree_with_cats = tag_words_with_categories(word_tree)
+    tag_tree_with_cats = tag_words_with_categories(tag_tree)
+    for i, word in enumerate(tokens):
+        tag = merge_tags(i, word, tag_tree_with_cats, word_tree_with_cats) 
+        tokens_with_slot_tags.append((word, tag))
+    found_query_focus = False
+    for i, item in enumerate(tokens_with_slot_tags):
+        word, tag = item
+        if tag in ['USER','MEDIA','NETWORK'] and not found_query_focus:
+            tokens_with_slot_tags[i] = (word, 'SEARCH')
+            found_query_focus = True
+        elif tag == UNK:
+            tokens_with_slot_tags[i] = (word, 'KEYWORD')
+    slots = {}
+    for word, tag in tokens_with_slot_tags:
+        if tag == 'SKIP':
+            continue
+        elif tag == 'KEYWORD':
+            if 'KEYWORDS' not in slots:
+                slots['KEYWORDS'] = []
+            slots['KEYWORDS'].append(word)
+        else:
+            if tag not in slots:
+                slots[tag] = word
+            else:
+                previous_words = slots[tag]
+                slots[tag] = ' '.join([previous_words, word])
+    return slots
+
+def merge_tags(i, word, tt, wt):
+    tt_ind = tt.leaf_treeposition(i)
+    wt_ind = wt.leaf_treeposition(i)
+    tt_pos = tt[tt_ind][1]
+    tt_category = tt[tt_ind][2]
+    wt_category = wt[wt_ind][2]
+    if wt_category == 'DATE':
+        return 'DATE_EXACT'
+    elif wt_category == 'DATE_FROM':
+        if tt_pos != 'IN':
+            return 'DATE_FROM'
+        else:
+            return SKIP
+    elif wt_category == 'DATE_TO':
+        if tt_pos != 'IN' and tt_pos != 'TO':
+            return 'DATE_TO'
+        else:
+            return SKIP
+    elif wt_category == 'LENGTH':
+        if tt_pos == 'CD' or tt_pos == 'LS':
+            return 'LENGTH_NUM'
+        elif tt_pos == 'NN' or tt_pos == 'NNS':
+            return 'LENGTH_UNIT'
+        else:
+            return SKIP
+    elif tt_pos == 'MEDIA':
+        return 'MEDIA'
+    elif tt_pos == 'NETWORK':
+        return 'NETWORK'
+    elif tt_pos == 'NETWORK_NAME':
+        return 'NETWORK_NAME'
+    elif tt_pos == 'USER':
+        return 'USER'
+    elif tt_category == "PosP":
+        if tt_pos != 'POS' and len(word) > 1:
+            return 'CREATOR'
+        else:
+            return SKIP
+    elif tt_category == 'PP':
+        if tt_pos != 'IN' and tt_pos != 'TO':
+            # get the value of preposition
+            j = i
+            tt_pos_j = tt_pos
+            while j > 0 and tt_pos_j != 'IN' and tt_pos_j != 'T0':
+                j = j-1
+                tt_ind_j = tt.leaf_treeposition(j)
+                tt_pos_j = tt[tt_ind_j][1]
+            wt_ind_j = wt.leaf_treeposition(j)
+            wt_category_j = wt[wt_ind_j][2]
+            if wt_category_j == 'BY' or wt_category_j == 'FROM':
+                return 'CREATOR'
+            else:
+                return UNK
+        else:
+            return SKIP
+    else:
+        return UNK
+
+def tag_words_with_categories(tree):
+    """
+    tag each word in the tree with the category it is listed under
+    for example, (LENGTH (NUM 1/1) minute/minute ) should have leaves
+    1/1/LENGTH minute/minute/LENGTH
+    """
+    for subtree in tree:
+        if isinstance(subtree, Tree):
+            for pos in subtree.treepositions('leaves'):
+                subtree[pos] += (subtree.node,)
+    return tree
+
 def compile_grammar(glist):
     grammar = ""
     gdict = {}
-    for tag, rule in glist:
+    for entry in glist:
+        tag, rules = entry[0], entry[1:]
+        rule_str = '\n'.join(rules)
         if tag in gdict:
-            if gdict[tag] != rule:
+            if gdict[tag] != rule_str:
                 raise ValueError("%s has two rules: '%s' and '%s'"
-                        %(tag, gdict[tag], rule) )
+                        %(tag, gdict[tag], rule_str) )
             else:
                 continue
         else:
-            gdict[tag] = rule
-            grammar += "%s: %s \n"%(tag, rule)
+            gdict[tag] = rule_str
+            grammar += "%s: %s \n"%(tag, rule_str)
     return grammar
 
 def tag_grammar():
     glist = [
-        ("MedP",    "{<DT>?<JJ>*<MEDIA>+<NN.*|NetP|NETWORK>*}"),
-        ("NetP",    "{<DT>?<JJ>*<NETWORK>+<NN.*|MedP|MEDIA>*}"),
-        ("PP",      "{<RB>?<IN|TO><[^IT].*>+}"),
-        ("PosP",    "{<.*><POS>}"),
+        ("MedP",    "{<DT>?<JJ>*<MEDIA>+<NN.*|NetP|NETWORK.*>*}"),
+        ("NetP",    "{<DT>?<JJ>*<NETWORK.*>+<NN.*|MedP|MEDIA>*}"),
+        ("PP",      "{<.*>+}", "<.*>}{<RB>?<IN|TO>", "}<WDT><.*>+{"),
+        ("PosP",    "{<.*><POS><NNS>?}"),
         ("QUOTE",   "{<``><.*>*<''>}"),
+        (UNK, "{<.*>}","}<MedP|NetP|PP|PosP|QUOTE>{")
     ]
     return glist
 
@@ -116,7 +225,9 @@ def word_grammar():
     ]
     rules = (
         NUM + [ ("LENGTH", "{<CMP>?<NUM|a><->?<UNIT><long>?}") ] +
-        DATE + [ ("DATE_FROM","{<FROM><DATE>}"), ("DATE_TO", "{<TO><DATE>}")]
+        DATE + [ ("DATE_FROM","{<FROM><DATE>}"), ("DATE_TO", "{<TO><DATE>}")] +
+        [ (UNK,   "{<.*>}",
+            "}<LENGTH|DATE.*|CMP|UNIT|FROM|TO|ON|IN|OF|ABOUT|BY|WITH>{") ]
         )
     return terminals + rules
 
@@ -192,13 +303,15 @@ def tag_domains(tagged, networks_json):
     network_list = get_network_names(networks_json)
     domain_tagged = []
     for word, tag in tagged:
-        if matches_domain(word, media_synset_list):
-            tag = 'MEDIA'
-        elif (matches_domain(word, network_synset_list)
-                or word in network_list ):
-            tag = 'NETWORK'
-        elif matches_domain(word, user_synset_list):
-            tag = 'USER'
+        if tag.startswith('N'):
+            if matches_domain(word, media_synset_list):
+                tag = 'MEDIA'
+            elif matches_domain(word, network_synset_list):
+                tag = 'NETWORK'
+            elif word in network_list:
+                tag = 'NETWORK_NAME'
+            elif matches_domain(word, user_synset_list):
+                tag = 'USER'
         domain_tagged.append((word, tag))
     return domain_tagged
 
